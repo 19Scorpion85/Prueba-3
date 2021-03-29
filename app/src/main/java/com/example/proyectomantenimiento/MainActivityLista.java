777d4ee9eb02e039b143caea07a93c0cd3c3672e
Andrés Eduardo Encina Tapia <escorpion@LAPTOP-VMITUE7E.(none)>
2021-03-29 01:18:29 -0300

    Cambios de diseño v3

---
+++ app/src/main/java/com/example/proyectomantenimiento/MainActivityLista.java
@@ -0,0 +1,14 @@
+package com.example.proyectomantenimiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lista);
+    }
+}
--- app/src/main/java/com/example/proyectomantenimiento/MainActivityMenu.java
+++ app/src/main/java/com/example/proyectomantenimiento/MainActivityMenu.java
@@ -2,13 +2,59 @@ package com.example.proyectomantenimiento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivityMenu extends AppCompatActivity {
+    Button entrar1;
+    Button entrar2;
+    Button entrar3;
+    Button entrar4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        entrar1=(Button)findViewById(R.id.btnMant);
+        entrar2=(Button)findViewById(R.id.btnRegistro);
+        entrar3=(Button)findViewById(R.id.btnLista);
+        entrar4=(Button)findViewById(R.id.btnDescarga);
+
+        entrar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent entrar=new Intent(MainActivityMenu.this,MainActivity1Motor.class);
+                    startActivity(entrar);
+
+            }
+        });
+
+        entrar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar2=new Intent(MainActivityMenu.this,MainActivityRegistrosNuevos.class);
+                startActivity(entrar2);
+            }
+        });
+
+        entrar3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar3=new Intent(MainActivityMenu.this,MainActivityLista.class);
+                startActivity(entrar3);
+            }
+        });
+
+        entrar4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar4=new Intent(MainActivityMenu.this,MainActivityDescarga.class);
+                startActivity(entrar4);
+            }
+        });
+
     }
 }
