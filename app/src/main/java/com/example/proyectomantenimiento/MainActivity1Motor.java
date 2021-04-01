140517fcb2a7553a2e5dcdf424d374da8595aaef
Andrés Eduardo Encina Tapia <escorpion@LAPTOP-VMITUE7E.(none)>
2021-04-01 14:12:55 -0300

    Comienzo diseño lista verificación 2

--- app/src/main/java/com/example/proyectomantenimiento/MainActivity1Motor.java
+++ app/src/main/java/com/example/proyectomantenimiento/MainActivity1Motor.java
@@ -2,13 +2,28 @@ package com.example.proyectomantenimiento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity1Motor extends AppCompatActivity {
+
+    Button entrar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity1_motor);
+
+        entrar1=(Button)findViewById(R.id.btnSiguiente5);
+
+        entrar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar=new Intent(MainActivity1Motor.this,MainActivity2EmbragueCaja.class);
+                startActivity(entrar);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/proyectomantenimiento/MainActivity2EmbragueCaja.java
@@ -0,0 +1,14 @@
+package com.example.proyectomantenimiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2EmbragueCaja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_embrague_caja);
+    }
+}
